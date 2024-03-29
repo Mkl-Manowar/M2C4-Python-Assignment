78a70f63d5495fcfd77e9fce0a6e6d3a63bae288
Mkl-Manowar <151755301+Mkl-Manowar@users.noreply.github.com>
2024-03-29 14:08:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import math
+
+#1
+list_one = ["1","carrot","onion","tomato",]
+tuple_one = (1,2,3,4,5)
+float_one = 2.2
+integer_one = 7
+decimal_one = 1.264365
+dictionary_one = {"one":1, "two":2, "three":3}
+
+#2
+rounded_float_one = math.ceil(float_one)
+
+#3
+square_root = math.sqrt(rounded_float_one)
+
+#4
+first_element = dictionary_one["one"]
+
+#5
+second_element = tuple_one[1]
+
+#6
+list_one += ["celery"]	
+
+#7
+list_one[0] = "potato"
+
+#8
+sorted_list = sorted(list_one)
+
+#9
+tuple_one += (6,)
+
+
+
+print(sorted_list)
+
+
+
+
